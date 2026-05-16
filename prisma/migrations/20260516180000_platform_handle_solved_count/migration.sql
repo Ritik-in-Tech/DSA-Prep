2c12da63614b2d9679d1595efb85c01251f57807
Ritik-in-Tech <rt936649@gmail.com>
2026-05-17 00:09:16 +0530

    added the skipped migration file

---
+++ prisma/migrations/20260516180000_platform_handle_solved_count/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "PlatformHandle" ADD COLUMN "solvedCount" INTEGER;
+ALTER TABLE "PlatformHandle" ADD COLUMN "solvedEasy" INTEGER;
+ALTER TABLE "PlatformHandle" ADD COLUMN "solvedMedium" INTEGER;
+ALTER TABLE "PlatformHandle" ADD COLUMN "solvedHard" INTEGER;
